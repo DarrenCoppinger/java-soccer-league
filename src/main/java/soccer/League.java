b99869c39baa74c46864b5996e6a3bb11b70a05d
DarrenCoppinger <darrencoppinger@gmail.com>
2021-08-31 02:34:17 +0100

    Add print output summary.

--- src/main/java/soccer/League.java
+++ src/main/java/soccer/League.java
@@ -21,11 +21,11 @@ public class League {
         player1.playerName = "George Eliot";
         
         Player player2 = new Player();
-        player1.playerName = "Graham Greene";
+        player2.playerName = "Graham Greene";
         
         
         Player player3 = new Player();
-        player1.playerName = "Geoffrey Chaucer";
+        player3.playerName = "Geoffrey Chaucer";
         Player[] thePlayers = {player1, player2, player3};
         
         Team team1 = new Team();
@@ -45,6 +45,22 @@ public class League {
         team2.playerArray[2].playerName = "Rafael Sabatini";
         
 
+        Game currGame = new Game();
+        currGame.homeTeam = team1;
+        currGame.awayTeam = team2;
+        
+        Goal goal1 = new Goal();
+        goal1.thePlayer = currGame.homeTeam.playerArray[2];
+        goal1.theTeam = currGame.homeTeam;
+        goal1.theTime = 55;
+        Goal[] theGoals = {goal1};
+        currGame.goals = theGoals;
+        
+        System.out.println("Goal scored after " + 
+                currGame.goals[0].theTime + " mins by " +
+                currGame.goals[0].thePlayer.playerName + " of " +
+                currGame.goals[0].theTeam.teamName);
+        
         
     }
     
